8e84a31c953631c19ddae880631812972a9cbdd2
Alex Ayala <66438806+alexayalauxd@users.noreply.github.com>
2020-11-28 17:59:45 -0600

    new-design-28-11

--- js/main.js
+++ js/main.js
@@ -99,6 +99,54 @@ inputs.forEach((input) => {
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
+	//POST----- ----- ----- ----- ----- ----- ----- -----
+
+	(async () => {
+		const rawResponse = await fetch('https://2o62j44a44.execute-api.us-east-1.amazonaws.com/dev/lead', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({name})
+		});
+		const content = await rawResponse.json();
+
+		console.log(content);
+	})();
+
+
+	// console.log('Me diste click');
+
+	// const datos = new FormData(formulario);
+
+	// console.log(datos);
+	// console.log(datos.get('name'));
+	// console.log(datos.get('lastname'));
+	// console.log(datos.get('email'));
+	// console.log(datos.get('phone'));
+
+	// fetch('https://2o62j44a44.execute-api.us-east-1.amazonaws.com/dev/lead',{
+	// 	method: 'POST',
+	// 	body: datos
+	// })
+	// .then( res => res.json())
+	// .then( data => {
+	// 	console.log(data);
+	// })
+
+	// const rawResponse = await fetch('https://2o62j44a44.execute-api.us-east-1.amazonaws.com/dev/lead', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Accept': 'application/json',
+	// 		'Content-Type': 'application/json'
+	// },
+	// 	body: JSON.stringify({a: 1, b: 'Textual content'})
+	// });
+
+	// const content = await rawResponse.json()
+
+
 	const terminos = document.getElementById('terminos');
 	if(campos.name && campos.lastname && campos.email && campos.phone && terminos.checked ){
 		formulario.reset();
@@ -114,4 +162,42 @@ formulario.addEventListener('submit', (e) => {
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
+
+
+	// const formData = new FormData(this);
+	// const searchParams = new URLSearchParams();
+
+	// for (const pair of formData) {
+	// 	searchParams.append(pair[0], pair[1]);
+	// }
+
+	// fetch('welcome.php', {
+	// 	method:'post',
+	// 	body: searchParams
+	// }). then(function (response) {
+	// 	return response.text();
+	// }).then(function (text) {
+	// 	console.log(text);
+	// }).catch(function (error) {
+	// 	console.log(error);
+	// })
+
+		// const rawResponse = new FormData(formulario);
+
+	// const rawResponse = await fetch('https://2o62j44a44.execute-api.us-east-1.amazonaws.com/dev/lead', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Accept': 'application/json',
+	// 		'Content-Type': 'application/json'
+	// 	},
+	// 	body: JSON.stringify({name})
+	// });
+
+	// .then( res => res.json())
+	// .then( data => {
+	// 	console.log(data);
+	// })
+
+	// const content = await rawResponse.json()
+
 });
